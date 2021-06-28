9fc4c17d44d110dfa664647a3fb29bc0c601faee
Kevin Leung <kevinresol@gmail.com>
2021-06-28 10:34:55 +0800

    cast arguments so their types won't be bound

--- src/why/Log.macro.hx
+++ src/why/Log.macro.hx
@@ -5,15 +5,20 @@ import haxe.macro.Expr;
 
 abstract Log(Any) {
 	public static macro function info(rest:Array<Expr>)
-		return macro @:pos(Context.currentPos()) why.Log.logger.log(why.Log.INFO, ${macro $a{rest}});
+		return forward('INFO', rest);
 	
 	public static macro function error(rest:Array<Expr>)
-		return macro @:pos(Context.currentPos()) why.Log.logger.log(why.Log.ERROR, ${macro $a{rest}});
+		return forward('ERROR', rest);
 	
 	public static macro function warn(rest:Array<Expr>)
-		return macro @:pos(Context.currentPos()) why.Log.logger.log(why.Log.WARN, ${macro $a{rest}});
+		return forward('WARN', rest);
 	
 	public static macro function debug(rest:Array<Expr>)
-		return macro @:pos(Context.currentPos()) why.Log.logger.log(why.Log.DEBUG, ${macro $a{rest}});
+		return forward('DEBUG', rest);
 	
+	static inline function forward(level:String, rest:Array<Expr>)
+		return macro @:pos(Context.currentPos()) why.Log.logger.log(why.Log.$level, $a{pack(rest)});
+	
+	static inline function pack(arr:Array<Expr>)
+		return arr.map(e -> macro cast $e);
 }
